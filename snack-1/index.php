67067190d8091e7722c9ed4fd3453eb0f0fb52b6
FabioLaForesta <fabiolaforesta@icloud.com>
2022-05-20 14:49:34 +0200

    finish

--- snack-1/index.php
+++ snack-1/index.php
@@ -8,33 +8,25 @@ Olimpia Milano - Cantù | 55-60
 $partite = [
 
         [
-            "squadraCasa" => "Messina",
-            "puntiCasa" => 20,
-            "squadraOspite" => "Catania",
-            "puntiOspite" => 30,
+            'squadraCasa' => 'Messina',
+            'puntiCasa' => '20',
+            'squadraOspite' => 'Catania',
+            'puntiOspite' => '30'
         ],
         [
-            "squadraCasa" => "Palermo",
-            "puntiCasa" => 23,
-            "squadraOspite" => "Enna",
-            "puntiOspite" => 22,
+            'squadraCasa' => 'Palermo',
+            'puntiCasa' => '23',
+            'squadraOspite' => 'Enna',
+            'puntiOspite' => '22'
         ],
         [
-            "squadraCasa" => "Trapani",
-            "puntiCasa" => 40,
-            "squadraOspite" => "Siracusa",
-            "puntiOspite" => 30,
+            'squadraCasa' => 'Trapani',
+            'puntiCasa' => '40',
+            'squadraOspite' => 'Siracusa',
+            'puntiOspite' => '30'
         ]
-];
-
-var_dump($partite);
-
-for ($i; $i < count($struttura); $i++){
-    $singoloAvvenimento = $struttura[$i];
-}
-
-var_dump($singoloAvvenimento);
-
+        ];
+//var_dump($partite);
 ?>
 
 <!DOCTYPE html>
@@ -47,17 +39,13 @@ var_dump($singoloAvvenimento);
 </head>
 <body>
     <div>
-        <h1>Prima partita</h1>
-        <h2>
-            <?php echo $partite['squadraCasa'] ?>
-        </h2>
-        Vs
-        <h2>
-            <?php echo $partite['squadraOspite'] ?>
-        </h2>
-        <h3>
-            Risultato: <?php echo $partite['puntiCasa'] ?> - <?php echo $partite['puntiOspite'] ?>
-        </h3>
+        <h1>Calendario stagione 2020-2021</h1>
+        <?php for ($i = 0; $i < count($partite); $i++){ ?>
+            <h3>
+                <?php echo $partite[$i]['squadraCasa']?> - <?php echo $partite[$i]['squadraOspite']?>
+            </h3>
+            <p>Risultato: <?php echo $partite[$i]['puntiCasa'] ?> | <?php echo $partite[$i]['puntiOspite'] ?></p>
+        <?php } ?>
     </div>
 </body>
 </html>

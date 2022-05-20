6f77f26b73d9f3d9ee34797b2a20e0e6d0b47737
FabioLaForesta <fabiolaforesta@icloud.com>
2022-05-20 16:12:20 +0200

    Push - 3 snack

---
+++ snack-3/index.php
@@ -0,0 +1,39 @@
+<!-- 
+    Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
+ -->
+
+<?php
+# Come genero in php un numero casuale? --> con RAND()
+// $numeroRandom = rand(1, 50);
+// var_dump($numeroRandom);
+
+# Mi creo un'array vuota dove inserirò i miei numeri:
+$new_array = [];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <p>Creo un'array con 15 numeri diversi casuali</p>
+
+        <?php 
+            while (count($new_array) < 15) { 
+                $numeroRandom = rand(1, 50);
+                    if(!in_array($numeroRandom, $new_array)){
+                        $new_array[] = $numeroRandom;
+                    }
+            };
+        ?>
+
+        <p>
+            <?php echo $new_array ?>
+        </p>
+
+    </body>
+</html>
